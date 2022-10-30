1ac02ee273f438e5cbe98ef10c5ad810a1de298e
git-suryacs <2suryacs@gmail.com>
2022-10-30 16:04:17 +0000

    Linkedlist learning

---
+++ Other_codes/linkedlist.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self,data) -> None:
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+
+    def printList(self):
+
+
+if __name__ == '__main__':
+    pass
